d03ee5b95b717b476e479b8036739ea6fdfd0585
Moisés Corrêa <moisesbrambilla@hotmail.com>
2023-05-08 16:13:16 -0300

    Criando tela de busca de receitas

--- lib/views/screens-home-page/search-screen.dart
+++ lib/views/screens-home-page/search-screen.dart
@@ -7,14 +7,43 @@ class SearchScreen extends StatefulWidget {
   State<SearchScreen> createState() => _SearchScreenState();
 }
 
-class _SearchScreenState extends State<SearchScreen> {
-  String _searchTerm = '';
+class Postagem {
+  final String imagem;
+  final String titulo;
+  final String descricao;
+  final int numeroCurtidas;
+  bool curtida;
 
-  void _onSearch(String searchTerm) {
-    setState(() {
-      _searchTerm = searchTerm;
-    });
-  }
+  Postagem({
+    required this.imagem,
+    required this.titulo,
+    required this.descricao,
+    this.curtida = false,
+    this.numeroCurtidas = 0,
+  });
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final List<Postagem> _postagens = [
+    Postagem(
+      imagem: 'lasanha-italiana.jpg',
+      titulo: 'Lasanha Italiana',
+      descricao:
+          'Aprenda a fazer uma deliciosa lasanha italiana com massa fresca e molho de tomate caseiro.',
+    ),
+    Postagem(
+      imagem: 'bolo-chocolate.jpg',
+      titulo: 'Bolo de Chocolate',
+      descricao:
+          'Aprenda a fazer um bolo de chocolate fofinho com cobertura de brigadeiro.',
+    ),
+    Postagem(
+      imagem: 'pizza-margherita.jpg',
+      titulo: 'Pizza Margherita',
+      descricao:
+          'Aprenda a fazer uma deliciosa pizza margherita com massa fina e crocante.',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +63,174 @@ class _SearchScreenState extends State<SearchScreen> {
           ),
         ),
       ),
+      body: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width - 60,
+              height: 50,
+              child: TextFormField(
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.search_outlined),
+                  labelStyle: TextStyle(
+                    color: Color(0xFF757575),
+                    fontWeight: FontWeight.bold,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.filter_list_alt,
+                color: Color(0xFF757575),
+                size: 32.0,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 20),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Text(
+                        'Receitas mais populares',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  itemCount: _postagens.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.75,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(15),
+                                topRight: Radius.circular(15),
+                              ),
+                              child: Image.asset(
+                                _postagens[index].imagem,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    _postagens[index].titulo,
+                                    maxLines: 1,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    _postagens[index].descricao,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(16, 8, 16, 16),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      IconButton(
+                                        icon: Icon(
+                                          _postagens[index].curtida
+                                              ? Icons.favorite
+                                              : Icons.favorite_border,
+                                          color: Colors.red,
+                                        ),
+                                        onPressed: () {
+                                          setState(() {
+                                            _postagens[index].curtida =
+                                                !_postagens[index].curtida;
+                                          });
+                                        },
+                                      ),
+                                      Text(
+                                        '${_postagens[index].numeroCurtidas} ${_postagens[index].numeroCurtidas == 1 ? "curtida" : "curtidas"}',
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.normal,
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  TextButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Ver receita',
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
